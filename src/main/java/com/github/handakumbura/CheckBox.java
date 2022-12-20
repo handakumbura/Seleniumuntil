bd3ef6e810432896ac7248cec6a1f636fbdcc0e4
dumiduh <dumiduh@gmail.com>
2022-12-20 19:36:53 +0530

    Initial commit CheckBox element wrapper.

---
+++ src/main/java/com/github/handakumbura/CheckBox.java
@@ -0,0 +1,59 @@
+/*
+Copyright 2022 Dumidu Handakumbura
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.handakumbura;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsElement;
+
+public class CheckBox implements WrapsElement {
+    private final WebElement element;
+
+    /***
+     *
+     * @param element WebElement instance for the checkbox.
+     */
+    public CheckBox(WebElement element) {
+        this.element = element;
+    }
+
+
+    /***
+     * Toggles the checkbox.
+     * @return The current instance of the WebElement.
+     */
+    public CheckBox toggle() {
+        element.click();
+        return this;
+    }
+
+    /***
+     * Un-Toggles the checkbox.
+     * @return The current instance of the WebElement.
+     */
+    public CheckBox unToggle() {
+        if (element.isSelected()) {
+            element.click();
+        }
+
+        return this;
+    }
+
+    /***
+     * A sugar method that can be used to improve readability of code.
+     * @return The current instance of the WebElement.
+     */
+    public CheckBox and() {
+        return this;
+    }
+
+    @Override
+    public WebElement getWrappedElement() {
+        return this.element;
+    }
+}
